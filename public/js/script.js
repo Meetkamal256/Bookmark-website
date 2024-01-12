b4b458a36aaa8595cc01c5128d5c1cbb3a09d25a
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-01-12 01:43:28 +0100

    download heading section done

--- public/js/script.js
+++ public/js/script.js
@@ -46,7 +46,8 @@ function onTabClick(e){
     // activate a new tab and panel based on the target
     e.target.classList.add('border-softRed', 'border-b-4');
     const classString = e.target.getAttribute('data-target');
-    document.getElementsByClassName(classString)[0]
+    document.getElementById('panels')
+    .getElementsByClassName(classString)[0]
     .classList.remove('hidden');
 }
 
